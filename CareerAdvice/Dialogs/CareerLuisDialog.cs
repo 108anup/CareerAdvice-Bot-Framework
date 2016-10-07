5c99e2db009e9898cdc076b6896b0044357d9195
Anup Agarwal <108anup@gmail.com>
2016-10-07 17:28:18 +0530

    Add LuisDialog Class

---
+++ CareerAdvice/Dialogs/CareerLuisDialog.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CareerAdvice.Dialogs
+{
+    public static partial class UserData
+    {
+        public const string Education = "education";
+        public const string Interests = "interests";
+        public const string Subjects = "subjects";
+    }
+
+    [Serializable]
+    public sealed class CareerLuisDialog : LuisDialog<object>
+    {
+    }
+}
